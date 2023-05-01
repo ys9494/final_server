0496084af5aa275a012f41e5e1fdbe253af4f2a1
Anjgmn <jermee315@gmail.com>
2023-05-01 12:13:51 +0900

    feat: 메인 전체 조회 1차

--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,11 @@
 const postService = require("./postService");
 const userService = require("./userService");
 const categoryService = require("./categoryService");
+const mainService = require("./mainService");
 
 module.exports = {
   postService,
   userService,
   categoryService,
+  mainService,
 };
