29cea69c289232119b363caef08a7ae12a52ebc1
Anjgmn <jermee315@gmail.com>
2023-05-01 12:13:51 +0900

    feat: 메인 전체 조회 1차

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,10 +2,12 @@ const postController = require("./postController");
 const userController = require("./userController");
 const categoryController = require("./categoryController");
 const authController = require("./authController");
+const mainController = require("./mainController");
 
 module.exports = {
   postController,
   userController,
   categoryController,
   authController,
+  mainController,
 };
