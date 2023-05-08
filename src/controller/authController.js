e94f3fdd68d3aeb78e2ee6b70240687db27765d7
[AI6/1반] 문석준 <anstjrdudwns88@gmail.com>
2023-05-08 11:36:26 +0900

    fix: 에러해결

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -3,13 +3,13 @@ const { getAuth, initPromise } = require("../config").firebase;
 const authController = {
   // Firebase 로그인
   loginUser: async (req, res, next) => {
-    const email = req.email;
+    const uid = req.uid;
     const idToken = req.idToken;
     const firebaseAuth = getAuth();
 
     try {
       await initPromise;
-      const userRecord = await firebaseAuth.getUserByEmail(email); // Firebase에서 이메일로 사용자 정보를 가져옵니다.
+      const userRecord = await firebaseAuth.getUser(uid); // Firebase에서 이메일로 사용자 정보를 가져옵니다.
 
       if (!userRecord) {
         return res.status(401).json({ message: "회원을 찾을 수 없습니다." });
