f2339b553bfb6ec02c9bd24115a26986711d85c1
ys9494 <mdlife94@gmail.com>
2023-05-12 10:48:38 +0900

    feat: config

--- src/app.js
+++ src/app.js
@@ -51,9 +51,9 @@ async function create() {
     });
   });
 
-  expressApp.use("/", (req, res, next) => {
-    res.send("hello world");
-  });
+  // expressApp.use("/", (req, res, next) => {
+  //   res.send("hello world");
+  // });
 
   // version 1의 api router를 등록
   expressApp.use("/api/v1", apiRouter.v1);
