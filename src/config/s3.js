dc52dff42da23b5a5686899e0243b013fcce3fb7
ys9494 <mdlife94@gmail.com>
2023-06-05 16:55:05 +0900

    feat: s3 config

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,7 +1,6 @@
 const AWS = require('aws-sdk')
 require("dotenv").config();
 
-
 AWS.config.update({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
